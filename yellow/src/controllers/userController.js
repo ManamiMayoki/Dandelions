51bcb4076208d9fe8011f0a5f20c2ea1fdd9c31c
ManamiMayoki <manamimarufa@gmail.com>
2026-08-10 22:58:03 +0600

    controller:Read operation

--- yellow/src/controllers/userController.js
+++ yellow/src/controllers/userController.js
@@ -1,22 +1,26 @@
-//The Brains
-const userService=require('../services/userService');
+let users=[
+    {id:1, name:"Marufa",email:"marufa@gmail.com"},
+    {id:2, name:"Meng",email:"meng@gmail.com"}
+]
 
-// Handler for GET /users
-const getAllUsers=async(req,res)=>{
-    const users=userService.fetchAllUsers();
-    res.status(200).json(users);
+
+
+//1.Read
+const getAllUsers=(req,res)=>{
+    req.status(200).json(users);
 }
 
-// Handler for POST /users
+//2.Create
 const createUser=(req,res)=>{
-    const newUserData=req.body;//incoming data parsed by express.json() middleware
+    const {name,email}=req.body;
 
-    if(!newUserData.name){
-        return res.status(400).json({error:"Name field is absolutely required"});
+    //validation
+    if(!name || !email){
+        return res.status(400).json({message:'Name & Email are required!'});
     }
-
-    const createdUser=userService.addNewUser(newUserData);
-    res.status(201).json(createdUser);
-};
-
-module.exports={getAllUsers,createUser};
+    const newUser={
+        id:users.length+1,
+        name,
+        email
+    };
+}
